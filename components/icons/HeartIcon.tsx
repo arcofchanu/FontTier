07dc82e01b2dc428473297d9086f10716dcc41dc
Ronit B Suthar <67582658+arcofchanu@users.noreply.github.com>
2025-09-22 15:47:22 +0530

    Update HeartIcon.tsx

--- components/icons/HeartIcon.tsx
+++ components/icons/HeartIcon.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 export const HeartIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M17.25 6.75H5.25a.75.75 0 00-.75.75v5.25a6.75 6.75 0 006.75 6.75h.75a6.75 6.75 0 006.75-6.75V7.5a.75.75 0 00-.75-.75z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M17.25 9h1.5a2.25 2.25 0 010 4.5h-1.5"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9 3v2.25M12 3v2.25M15 3v2.25"
+    />
   </svg>
 );
+
